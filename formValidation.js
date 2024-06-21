e1fb3d0b6319cad15ce162295e139cf3755f12b5
Bfemeng <92241705+bfemeng@users.noreply.github.com>
2024-06-20 22:02:24 -0400

    Fixed submit button to show alert

--- formValidation.js
+++ formValidation.js
@@ -1,10 +1,13 @@
 function validateForm() {
-    const email = document.getElementById('email').value;
-    const confirmEmail = document.getElementById('email_verification').value;
-    if (email !== confirmEmail) {
-      alert('Whoops! Email addresses do not match!');
-      return false; // prevent form submission
-    }
-    return true; // allow form submission
-  }
+  return new Promise((resolve, reject) => {
+      const email = document.getElementById('email').value;
+      const confirmEmail = document.getElementById('email_verification').value;
+      
+      if (email !== confirmEmail) {
+          alert('Whoops! Email addresses do not match!');
+      } else {
+          resolve('Emails match');
+      }
+  });
+}
 
